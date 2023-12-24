e88a23274469c962331d49c9d7846bd942b2cc80
Hunter Burningham <hunter.d.burningham@gmail.com>
2023-12-24 00:33:40 -0700

    media query fix expectant mothers testimonial

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,6 +6,18 @@
 
 (function($) {
 
+	// Passive event listeners
+jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+};
+
 	var	$window = $(window),
 		$head = $('head'),
 		$body = $('body');
